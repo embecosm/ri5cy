2a430d57f0fef80a8f0841ee5c8cd36ce73130d3
Ian Bolton <ian.bolton@embecosm.com>
2017-07-09 06:27:53 +0100

    Fix dp_ram for memory accesses of bytes and half-words.
    Files Changed:
    	* verilator-model/dp_ram.sv: Handle stores and loads of
    	bytes and half-words correctly.

--- verilator-model/dp_ram.sv
+++ verilator-model/dp_ram.sv
@@ -54,23 +54,88 @@ module dp_ram
     rdata_a_o[112+: 8] <= mem[addr_a_i + 14];
     rdata_a_o[120+: 8] <= mem[addr_a_i + 15];
 
+    /* addr_b_i is the actual memory address referenced */
+
     if (en_b_i && we_b_i)
     begin
-      if (be_b_i[0])
-        mem[addr_b_i + 0] <= wdata_b_i[7:0];
-      if (be_b_i[1])
-        mem[addr_b_i + 1] <= wdata_b_i[15:8];
-      if (be_b_i[2])
+      /* full word being stored */
+      if (be_b_i[3:0] == 4'b1111)
+      begin
+        mem[addr_b_i + 0] <= wdata_b_i[ 7: 0];
+        mem[addr_b_i + 1] <= wdata_b_i[15: 8];
         mem[addr_b_i + 2] <= wdata_b_i[23:16];
-      if (be_b_i[3])
         mem[addr_b_i + 3] <= wdata_b_i[31:24];
-    end
+      end
+      /* single byte being stored */
+      if (be_b_i[3:0] == 4'b0001)
+      begin
+        mem[addr_b_i + 0] <= wdata_b_i[ 7: 0];
+      end
+      if (be_b_i[3:0] == 4'b0010)
+      begin
+        mem[addr_b_i + 0] <= wdata_b_i[15: 8];
+      end
+      if (be_b_i[3:0] == 4'b0100)
+      begin
+        mem[addr_b_i + 0] <= wdata_b_i[23:16];
+      end
+      if (be_b_i[3:0] == 4'b1000)
+      begin
+        mem[addr_b_i + 0] <= wdata_b_i[31:24];
+      end
+      /* half-word being stored */
+      if (be_b_i[3:0] == 4'b0011)
+      begin
+        mem[addr_b_i + 0] <= wdata_b_i[ 7: 0];
+        mem[addr_b_i + 1] <= wdata_b_i[15: 8];
+      end
+      if (be_b_i[3:0] == 4'b1100)
+      begin
+        mem[addr_b_i + 0] <= wdata_b_i[23:16];
+        mem[addr_b_i + 1] <= wdata_b_i[31:24];
+      end
+   end
 
-    rdata_b_o[ 7: 0] <= mem[addr_b_i + 0];
-    rdata_b_o[15: 8] <= mem[addr_b_i + 1];
-    rdata_b_o[23:16] <= mem[addr_b_i + 2];
-    rdata_b_o[31:24] <= mem[addr_b_i + 3];
-  end
+   if (en_b_i && we_b_i == 1'b0)
+   begin
+     /* full word load */
+     if (be_b_i[3:0] == 4'b1111)
+     begin
+       rdata_b_o[ 7: 0] <= mem[addr_b_i + 0];
+       rdata_b_o[15: 8] <= mem[addr_b_i + 1];
+       rdata_b_o[23:16] <= mem[addr_b_i + 2];
+       rdata_b_o[31:24] <= mem[addr_b_i + 3];
+     end
+     /* single byte being loaded */
+     if (be_b_i[3:0] == 4'b0001)
+     begin
+       rdata_b_o[ 7: 0] <= mem[addr_b_i + 0];
+     end
+     if (be_b_i[3:0] == 4'b0010)
+     begin
+       rdata_b_o[15: 8] <= mem[addr_b_i + 0];
+     end
+     if (be_b_i[3:0] == 4'b0100)
+     begin
+       rdata_b_o[23:16] <= mem[addr_b_i + 0];
+     end
+     if (be_b_i[3:0] == 4'b1000)
+     begin
+       rdata_b_o[31:24] <= mem[addr_b_i + 0];
+     end
+     /* half-word being loaded */
+     if (be_b_i[3:0] == 4'b0011)
+     begin
+       rdata_b_o[ 7: 0] <= mem[addr_b_i + 0];
+       rdata_b_o[15: 8] <= mem[addr_b_i + 1];
+     end
+     if (be_b_i[3:0] == 4'b1100)
+     begin
+       rdata_b_o[23:16] <= mem[addr_b_i + 0];
+       rdata_b_o[31:24] <= mem[addr_b_i + 1];
+     end
+   end
+   end
 
   function [7:0] readByte;
     /* verilator public */
